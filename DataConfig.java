037b65c211913bc3564704c2aaf8c6b5bd848b2d
editarul <editarul@in.ibm.com>
2019-03-21 18:23:28 +0530

    Add files via upload

---
+++ DataConfig.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.att.gcp.internal.data;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author EdithArulJaneA
+ *
+ */
+@Configuration
+public class DataConfig {
+	/*@Bean
+	@Profile({"DEV", "default"})
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource devDataSource() {
+		return DataSourceBuilder.create().build();
+
+	}*/
+}
